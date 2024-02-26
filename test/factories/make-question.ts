885c7c9b5f4999f86e1a8c70795e5cf68fcdcdee
Raul Garcia <raul.gsouza@gmail.com>
2024-02-26 19:49:55 -0300

    Feat: Added fake data to question factory

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,16 +1,24 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityId(),
-    content: 'Question Content',
-    title: 'Question Title',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId(),
+      content: faker.lorem.sentence(),
+      title: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
