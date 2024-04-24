44cdb46948e3d7855d16b4a26275025bfa92d925
Raul Garcia <raul.gsouza@gmail.com>
2024-04-23 21:38:53 -0300

    feat: choose best answer

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
@@ -0,0 +1,52 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { ChooseQuestionBestAnswerUseCase } from './choose-question-best-answer'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { Answer } from '../../enterprise/entities/answer'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: ChooseQuestionBestAnswerUseCase
+
+describe('Choose question best answer', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new ChooseQuestionBestAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryQuestionsRepository,
+    )
+  })
+
+  it('should be able to choose the question best answer', async () => {
+    const question = makeQuestion()
+    const answer = makeAnswer({ questionId: question.id })
+
+    await inMemoryQuestionsRepository.create(question)
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: question.authorId.toString(),
+    })
+
+    expect(inMemoryQuestionsRepository.items[0].bestAnswerId).toEqual(answer.id)
+  })
+
+  it('should not be able to choose another users question best answer', async () => {
+    const question = makeQuestion({ authorId: new UniqueEntityId('author-1') })
+    const answer = makeAnswer({ questionId: question.id })
+
+    await inMemoryQuestionsRepository.create(question)
+    await inMemoryAnswersRepository.create(answer)
+
+    expect(() =>
+      sut.execute({
+        answerId: answer.id.toString(),
+        authorId: 'author-2',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -0,0 +1,50 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Question } from '../../enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface ChooseQuestionBestAnswerUseCaseRequest {
+  answerId: string
+  authorId: string
+}
+
+interface ChooseQuestionBestAnswerUseCaseResponse {
+  question: Question
+}
+
+export class ChooseQuestionBestAnswerUseCase {
+  constructor(
+    private answersRepository: AnswersRepository,
+    private questionsRepository: QuestionsRepository,
+  ) {}
+
+  async execute({
+    answerId,
+    authorId,
+  }: ChooseQuestionBestAnswerUseCaseRequest): Promise<ChooseQuestionBestAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Could not find answer.')
+    }
+
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString(),
+    )
+
+    if (!question) {
+      throw new Error('Could not find question.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    question.bestAnswerId = answer.id
+
+    await this.questionsRepository.save(question)
+
+    return { question }
+  }
+}
